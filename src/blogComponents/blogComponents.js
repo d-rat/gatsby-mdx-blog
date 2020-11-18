649008ff40bdf49f22cadb9e85efaf5832b95235
Dilip <dilipsinghdangwal@gmail.com>
2020-11-18 10:39:21 +0530

    added cutomized markdown components.

---
+++ src/blogComponents/blogComponents.js
@@ -0,0 +1,45 @@
+import React from "react"
+
+function primaryHeading(props) {
+  return <h1 className="text-4xl font-bold">{props.children}</h1>
+}
+
+function secondaryHeading(props) {
+  return <h1 className="text-2xl font-semibold">{props.children}</h1>
+}
+
+function paragraph(props) {
+  return <h1 className="text-xl">{props.children}</h1>
+}
+
+function codeBlock(props) {
+  return (
+    <div className="px-4 py-4 bg-gray-900 rounded">
+      <h1 className="text-white ">{props.children}</h1>
+    </div>
+  )
+}
+function inlineCode(props) {
+  return (
+    <span className="px-2 font-semibold bg-gray-300 rounded text-deeppink-600">
+      {props.children}
+    </span>
+  )
+}
+
+function blockQuote(props) {
+  return (
+    <div className="px-8 py-4 ">
+      <h1 className="font-semibold text-purple-800 ">{props.children}</h1>
+    </div>
+  )
+}
+
+export {
+  primaryHeading,
+  secondaryHeading,
+  paragraph,
+  codeBlock,
+  blockQuote,
+  inlineCode,
+}
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,15 +1,30 @@
 import React from "react"
+import Header from "../components/header"
+import Footer from "../components/footer"
 import { graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import * as DesignSystem from "../blogComponents/blogComponents"
 
 function BlogTemplate({ data: { mdx } }) {
   return (
-    <div className="mx-auto max-w-customWidth">
-      <h1>{mdx.frontmatter.title}</h1>
-      <MDXProvider>
-        <MDXRenderer>{mdx.body}</MDXRenderer>
-      </MDXProvider>
+    <div>
+      <Header />
+      <div className="mx-auto max-w-customWidth">
+        <MDXProvider
+          components={{
+            h1: DesignSystem.primaryHeading,
+            h2: DesignSystem.secondaryHeading,
+            p: DesignSystem.paragraph,
+            code: DesignSystem.codeBlock,
+            blockquote: DesignSystem.blockQuote,
+            inlineCode: DesignSystem.inlineCode,
+          }}
+        >
+          <MDXRenderer>{mdx.body}</MDXRenderer>
+        </MDXProvider>
+      </div>
+      <Footer />
     </div>
   )
 }
@@ -20,9 +35,6 @@ export const pageQuery = graphql`
   query MyQuery($slug: String!) {
     mdx(frontmatter: { slug: { eq: $slug } }) {
       body
-      frontmatter {
-        title
-      }
     }
   }
 `
